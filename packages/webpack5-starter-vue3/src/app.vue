626423d6548a2997d2da37be7a4c32b40fa3ab0f
hadeshe93 <hadeshe93@gmail.com>
2022-05-31 10:44:29 +0800

    初始化项目

---
+++ packages/webpack5-starter-vue3/src/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="view-app">
+    count 值: {{ cnt }} 
+    <button @click="addCnt">Click</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const cnt = ref(0);
+const addCnt = () => {
+  cnt.value += 1;
+  console.log('cnt value: ', cnt);
+};
+</script>
+
+<style lang="scss">
+.view-app {
+  button {
+    width: 80px;
+    height: 30px;
+    padding: 0;
+    line-height: 30px;
+    text-align: center;
+  }
+}
+</style>

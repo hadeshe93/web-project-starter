9851a62bb3ec4bbbef233ee010155e1a6abda4f3
hadeshe93 <hadeshe93@gmail.com>
2022-06-28 10:00:33 +0800

    feat: 完善 demo 项目

---
+++ packages/webpack5-starter-vue3-ts/configs/webpack.config.js
@@ -0,0 +1,74 @@
+/*
+ * @Description   : webpack 构建配置
+ * @usage         : 
+ * @Date          : 2022-04-19 09:30:01
+ * @Author        : hadeshe93<hadeshe93@gmail.com>
+ * @LastEditors   : hadeshe
+ * @LastEditTime  : 2022-06-27 14:24:23
+ * @FilePath      : /webpack5-starter/packages/webpack5-starter-vue3-ts/configs/webpack.config.js
+ */
+
+const { resolve: pathResolve } = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const MODE_DEVELOPMENT = 'development';
+
+const resolve = (pathname) => pathResolve(__dirname, '../', pathname);
+const mode = process.env.MODE || MODE_DEVELOPMENT;
+const isDevMode = mode === MODE_DEVELOPMENT;
+const styleLoader = isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader;
+
+const config = {
+  mode,
+  entry: {
+    app: resolve('src/main.ts'),
+  },
+  module: {
+    rules: [
+      { test: /\.vue$/, loader: 'vue-loader' },
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [styleLoader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [styleLoader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(png|svg|jpg|jpeg|gif|eot|svg|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'assets/images/[name].[hash].[ext]',
+              esModule: false
+            }
+          }
+        ],
+        type: 'javascript/auto'
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    port: 8200,
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    ...( isDevMode ? [] : [new MiniCssExtractPlugin()] ),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: resolve('public/index.html'),
+    }),
+  ],
+}
+
+module.exports = config;
---
+++ packages/webpack5-starter-vue3-ts/src/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="view-app">
+    count 值: {{ cnt }} 
+    <button @click="addCnt">Click</button>
+
+    <Pannel title="大漠孤烟直" content="君不见黄河之水天上来君不见黄河之水天上来君不见黄河之水天上来君不见黄河之水天上来" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Pannel from './components/pannel.vue';
+
+const cnt = ref(0);
+const addCnt = () => {
+  cnt.value += 1;
+  console.log('cnt value: ', cnt);
+};
+</script>
+
+<style lang="scss">
+.view-app {
+  button {
+    width: 80px;
+    height: 30px;
+    padding: 0;
+    line-height: 30px;
+    text-align: center;
+  }
+}
+</style>

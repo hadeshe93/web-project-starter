7a81acbc4cf330dc3c33e34c02631e6d20c0b143
hadeshe93 <hadeshe93@gmail.com>
2022-08-15 17:25:18 +0800

    feat: 修正 react-cms 模板的项目配置

--- packages/webpack5-react-cms/src/pages/demo1/project.config.js
+++ packages/webpack5-react-cms/src/pages/demo1/project.config.js
@@ -2,8 +2,9 @@ module.exports = {
   page: {
     title: 'react 标题',
     description: 'react 描述',
+    useFlexible: false,
   },
   build: {
-    frameworkType: 'react',
+    frameworkType: 'react-cms',
   },
 };

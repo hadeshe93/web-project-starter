9295feba32d5482e0b1c42c820c1136dccda90eb
hadeshe93 <hadeshe93@gmail.com>
2022-12-29 21:13:18 +0800

    fix: 完善构建配置

--- packages/webpack5-vue3/src/pages/demo1/project.config.js
+++ packages/webpack5-vue3/src/pages/demo1/project.config.js
@@ -4,23 +4,8 @@ module.exports = {
     description: 'vue 描述',
   },
   build: {
-    frameworkType: 'vue',
-    useBundleAnalyzer: {
-      analyzerMode: 'static',
-    },
   },
-  plugins: {
-    webpackConfigHooks: [{
-      pluginName: 'selfPlugin',
-      hooks: {
-        devServer(options) {
-          if (options) {
-            // https://webpack.js.org/configuration/dev-server/#devserverhistoryapifallback
-            options.historyApiFallback = true;
-          }
-          return options;
-        },
-      },
-    }],
-  },
+  middlewares: [
+    ['@hadeshe93/wpconfig-mw-vue3']
+  ],
 };

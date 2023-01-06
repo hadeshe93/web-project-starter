0520e10c5f8e432d795b0c605def3289b7774730
hadeshe93 <hadeshe93@gmail.com>
2023-01-06 18:59:43 +0800

    feat: 更新模板

--- packages/webpack5-vue3-element/src/pages/mainapp-qiankun/project.config.js
+++ packages/webpack5-vue3-element/src/pages/mainapp-qiankun/project.config.js
@@ -1,64 +1,42 @@
-const { VueLoaderPlugin } = require('vue-loader');
 const AutoImport = require('unplugin-auto-import/webpack');
 const Components = require('unplugin-vue-components/webpack');
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
+const { defineProjectConfig } = require('@hadeshe93/builder-core');
 
-module.exports = {
+module.exports = defineProjectConfig({
   page: {
-    title: 'vue 标题',
-    description: 'vue 描述',
-    useFlexible: false,
+    title: 'QIANKUN',
+    description: 'QIANKUN Description',
   },
   build: {
-    frameworkType: 'vue-element'
   },
-  plugins: {
-    webpackConfigHooks: [
-      {
-        pluginName: 'autoImport',
-        hooks: {
-          output(options) {
-            options.publicPath = '/';
-            return options;
-          },
-          devServer(options) {
-            options.port = 3002;
-            options.allowedHosts = 'all';
-            // https://webpack.js.org/configuration/dev-server/#devserverhistoryapifallback
-            options.historyApiFallback = true;
-            return options;
-          },
-          stats(options = {}) {
-            options.children = true;
-            options.errorDetails = true;
-            return options;
-          },
-          async plugins(plugins) {
-            const posToInsert = plugins.findIndex((plugin) => plugin instanceof VueLoaderPlugin) + 1;
-            plugins.splice(
-              posToInsert,
-              0,
-              // @ts-ignore
-              AutoImport({
-                resolvers: [ElementPlusResolver()]
-              }),
-              // @ts-ignore
-              Components({
-                resolvers: [ElementPlusResolver()]
-              })
-            );
-            return plugins;
-          },
-          beforeMerge(options) {
-            options.target = 'web';
-            return options;
-          },
-          afterMerge(options) {
-            console.log('options: ', JSON.stringify(options, null, 2));
-            return options;
-          },
-        }
-      }
+  middlewares: [
+    ['@hadeshe93/wpconfig-mw-vue3'],
+    [
+      () => (chainConfig) => {
+        const VUE_LOADER_PLUGIN_NAME = 'VueLoaderPlugin';
+        const UNPLUGIN_AUTO_IMPORT_NAME = 'unplugin-auto-import';
+        // 该插件不能用 new 实例化，所有要定制初始化方法
+        chainConfig.plugin(UNPLUGIN_AUTO_IMPORT_NAME)
+          .before(VUE_LOADER_PLUGIN_NAME)
+          .use(AutoImport, [{
+            resolvers: [ElementPlusResolver()]
+          }]);
+        chainConfig.plugin(UNPLUGIN_AUTO_IMPORT_NAME)
+          .init((Plugin, args) => Plugin(...args));
+
+        const UNPLUGIN_VUE_COMPONENTS_NAME = 'unplugin-vue-components';
+        // 该插件不能用 new 实例化，所有要定制初始化方法
+        chainConfig.plugin(UNPLUGIN_VUE_COMPONENTS_NAME)
+          .before(VUE_LOADER_PLUGIN_NAME)
+          .use(Components, [{
+            resolvers: [ElementPlusResolver()]
+          }]);
+        chainConfig.plugin(UNPLUGIN_VUE_COMPONENTS_NAME)
+          .init((Plugin, args) => Plugin(...args));
+
+        return chainConfig;
+      },
     ]
-  }
-};
+  ],
+});
